d7c01bd40c3e251da9232bb62d347daf24a53778
vladimir <grinfeldsv75@yahoo.com>
2024-12-31 16:40:44 -0500

    adding functionality

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -9,27 +9,45 @@ const CartItem = ({ onContinueShopping }) => {
 
   // Calculate total amount for all products in the cart
   const calculateTotalAmount = () => {
- 
+    let tCost =0;
+    cart.forEach(item => {
+        const iCost = parseFloat(item.cost.replace('$',''));
+        tCost = tCost + (iCost* item.quantity)
+    });
+    return tCost;
   };
 
   const handleContinueShopping = (e) => {
-   
+   onContinueShopping(e);
   };
 
-
+  const handleCheckoutShopping = (e) => {
+      alert('Functionality to be added for future reference');
+  };
 
   const handleIncrement = (item) => {
+    const aItem ={...item}
+    aItem.quantity++;
+    dispatch(updateQuantity(aItem));
   };
 
   const handleDecrement = (item) => {
-   
+   const dItem ={...item}
+   dItem.quantity++;
+    dispatch(updateQuantity(dItem));
   };
 
   const handleRemove = (item) => {
+    //console.log("handleRemove called");
+    dispatch(removeItem(item));
   };
 
   // Calculate total cost based on quantity for an item
   const calculateTotalCost = (item) => {
+    let tCost =0;
+    const iCost = parseFloat(item.cost.replace('$',''));
+    tCost = tCost + (iCost* item.quantity)
+    return tCost;
   };
 
   return (
@@ -57,7 +75,7 @@ const CartItem = ({ onContinueShopping }) => {
       <div className="continue_shopping_btn">
         <button className="get-started-button" onClick={(e) => handleContinueShopping(e)}>Continue Shopping</button>
         <br />
-        <button className="get-started-button1">Checkout</button>
+        <button className="get-started-button1" onClick={(e) => handleCheckoutShopping(e)}>Checkout</button>
       </div>
     </div>
   );
--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -7,12 +7,27 @@ export const CartSlice = createSlice({
   },
   reducers: {
     addItem: (state, action) => {
-    
+    const { name, image, cost } = action.payload;
+    const existingItem = state.items.find(item => item.name === name);
+        if (existingItem) {
+            existingItem.quantity++;
+        } else {
+            state.items.push({ name, image, cost, quantity: 1 });
+        }
     },
     removeItem: (state, action) => {
+        //console.log("removeItem from Slice called");
+        const { name, image, cost } = action.payload;
+        //console.log("const from Slice called"+name);
+        state.items = state.items.filter(item => item.name !== name)
+        //console.log("State from Slice called");
     },
     updateQuantity: (state, action) => {
-
+        const { name, quantity } = action.payload;
+        const itemToUpdate = state.items.find(item => item.name === name);
+        if (itemToUpdate) {
+            itemToUpdate.quantity = quantity;
+        }
     
     },
   },
